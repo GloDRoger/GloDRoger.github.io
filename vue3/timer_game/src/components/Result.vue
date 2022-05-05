d305409578823f2ad92078a7a33ab96cbc6978c1
GloDRoger <103984845+GloDRoger@users.noreply.github.com>
2022-05-05 13:04:18 +0800

    Update Result.vue

--- vue3/timer_game/src/components/Result.vue
+++ vue3/timer_game/src/components/Result.vue
@@ -1,8 +1,6 @@
 <template>
-    <div>
-        <h2>游戏结束！</h2>
-        <p>你的反应时间是：{{time}}秒</p>
-    </div>
+    <n-result status="418" title="游戏结束！" :description="`你的反应时间是：${time}秒`">
+    </n-result>
 </template>
 
 <script setup>
@@ -21,7 +19,7 @@ div{
     height: 400px;
     background-color:#fff;
     margin: 50px auto 0;
-    padding-top: 15%;
+    padding-top: 10%;
     box-sizing: border-box;
     box-shadow: 0 0 5px 5px rgb(146, 145, 145);
 }
